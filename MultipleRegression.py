b4c961dac744085674647ece461926ed10900b68
Takumi Shine <80245766+ShineTakumi@users.noreply.github.com>
2021-05-31 23:05:30 +0900

    Update MultipleRegression.py

--- MultipleRegression.py
+++ MultipleRegression.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
@@ -6,22 +7,25 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
+sns.set() # seabornのスタイルをセット
 
 # 糖尿病患者のデータセットをロード
 dataset = datasets.load_diabetes()
 
 # 説明変数
 X = dataset.data
-np.savetxt('X.csv',X,delimiter=',')
+
+# 相関の高い4番目～7番目の説明変数を除外
+X = np.hstack((X[:, 0:5], X[:, 8:10]))
+
 # 目的変数
 y = dataset.target
-np.savetxt('y.csv',y,delimiter=',')
 
 # 学習用、テスト用にデータを分割（1:1）
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.7, random_state=0)
 
-# 予測モデルを作成(重回帰)
-clf = linear_model.LinearRegression()
+# 予測モデルを作成(リッジ回帰)
+clf = linear_model.Ridge()
 
 # 学習
 clf.fit(X_train, y_train)
@@ -35,11 +39,3 @@ print("回帰係数:", a)
 print("切片:", b) 
 print("決定係数(学習用):", clf.score(X_train, y_train))
 print("決定係数(テスト用):", clf.score(X_test, y_test))
-
-for i in range(X.shape[1]):
-    plt.plot(X[:, i], y, 'o')
-
-plt.xlabel("X")
-plt.ylabel("y")
-plt.grid(True)
-plt.show()
